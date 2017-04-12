82aa8baab2f0bbb9cccf081e5e8969485c1198ab
Alonso Ugalde <augaldecr@gmail.com>
2017-04-12 10:14:36 -0600

    Eliminados los label del formulario de registro de usuario nuevo

---
+++ RegistroDocente/RegistroDocente/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,15 @@
+﻿using RegistroDocente.ViewModels;
+
+namespace RegistroDocente.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+
+    }
+}
