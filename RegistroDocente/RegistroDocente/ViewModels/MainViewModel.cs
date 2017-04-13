65a776028887303367d6a74741dc07ab3ec5d09e
Alonso Ugalde <augaldecr@gmail.com>
2017-04-13 11:50:49 -0600

    Implementado MVVM

--- RegistroDocente/RegistroDocente/ViewModels/MainViewModel.cs
+++ RegistroDocente/RegistroDocente/ViewModels/MainViewModel.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace RegistroDocente.ViewModels
 {
     public class MainViewModel
     {
+        #region Attributtes
+        public ICommand Periodos { get; set; }
+        public ICommand Instituciones { get; set; }
+        public ICommand Asignaturas { get; set; }
+        public ICommand Secciones { get; set; }
+        public ICommand Horarios { get; set; }
+        public ICommand Porcentajes { get; set; }
+        public ICommand Ajustes { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            Periodos = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso","Tu puta madre","Aceptar");
+            });
+            Instituciones = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso", "Tu puta madre", "Aceptar");
+            });
+            Asignaturas = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso", "Tu puta madre", "Aceptar");
+            });
+            Secciones = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso", "Tu puta madre", "Aceptar");
+            });
+            Horarios = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso", "Tu puta madre", "Aceptar");
+            });
+            Porcentajes = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso", "Tu puta madre", "Aceptar");
+            });
+            Ajustes = new Command(() => {
+                App.Current.MainPage.DisplayAlert("Aviso", "Tu puta madre", "Aceptar");
+            });
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
     }
 }
